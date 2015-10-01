81b15bea37bf96ecbb9c4bbb5125a18ca2a24dbf
Angela Deady <adeady@i-a-i.com>
2015-10-01 11:11:55 -0400

    refactor ui a bit

--- ui/src/main/java/demo/DemoApplication.java
+++ ui/src/main/java/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
@@ -10,21 +9,14 @@ import org.springframework.cloud.security.oauth2.sso.OAuth2SsoConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @SpringBootApplication
-@RestController
 @EnableZuulProxy
 @EnableOAuth2Sso
 public class DemoApplication {
-
 
 
     @Configuration
@@ -38,9 +30,12 @@ public class DemoApplication {
 
         @Override
         public void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().antMatchers("/index.html", "/home.html", "/")
-                    .permitAll().anyRequest().authenticated().and().csrf()
-                    .csrfTokenRepository(csrfTokenRepository()).and()
+            http
+                    .authorizeRequests().antMatchers("/index.html", "/home.html", "/").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .csrf().csrfTokenRepository(csrfTokenRepository())
+                    .and()
                     .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
         }
 
