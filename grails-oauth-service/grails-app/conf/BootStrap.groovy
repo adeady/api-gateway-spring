b1c261c8871e10d693db2ce2be0eaba6a68bdef9
Angela Deady <adeady@i-a-i.com>
2015-10-05 10:54:37 -0400

    start of an oauth grails service

---
+++ grails-oauth-service/grails-app/conf/BootStrap.groovy
@@ -0,0 +1,33 @@
+import com.iai.Client
+import com.iai.User
+import com.iai.UserRole
+import com.iai.Role
+
+class BootStrap {
+
+    def init = { servletContext ->
+        Role roleUser = new Role(authority: 'ROLE_USER').save(flush: true)
+
+        User user = new User(
+                username: 'user',
+                password: 'password',
+                enabled: true,
+                accountExpired: false,
+                accountLocked: false,
+                passwordExpired: false
+        ).save(flush: true)
+
+        UserRole.create(user, roleUser, true)
+
+        new Client(
+                clientId: 'my-client',
+                authorizedGrantTypes: ['authorization_code', 'refresh_token', 'implicit', 'password', 'client_credentials'],
+                authorities: ['ROLE_CLIENT'],
+                scopes: ['read', 'write'],
+                redirectUris: ['http://myredirect.com']
+        ).save(flush: true)
+
+    }
+    def destroy = {
+    }
+}
